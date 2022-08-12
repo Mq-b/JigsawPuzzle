13f6945164065de9c9fe84c02b6daca721f38bca
guiguli <3326284481@qq.com>
2022-08-12 20:11:44 +0800

    初始化菜单同时封装之前初始化界面的代码为函数

--- src/App.java
+++ src/App.java
@@ -7,9 +7,9 @@ public class App {
         //表示程序的启动入口
 
         //如果我们想要开启一个界面，就创建谁的对象就可以
-        new LoginJframe();
+        //new LoginJframe();
 
-        new RegisterJFrame();
+        //new RegisterJFrame();
 
         new GameJFrame();
     }
--- src/com/github/guiguli/GameJFrame.java
+++ src/com/github/guiguli/GameJFrame.java
@@ -9,6 +9,47 @@ public class GameJFrame extends JFrame {
     //以后跟游戏相关的所有逻辑都写在这个类中
 
     public GameJFrame(){
+        //初始化界面
+        initJFram();
+
+        //初始化菜单
+        initJMenuBar();
+
+        //设置界面显示
+        this.setVisible(true);
+    }
+
+    private void initJMenuBar() {
+        //创建整个的菜单对象
+        JMenuBar jMenuBar = new JMenuBar();
+
+        //创建菜单上面的两个选项的对象(功能 关于我们)
+        JMenu functionJMenu = new JMenu("功能");
+        JMenu aboutJMenu = new JMenu("关于我们");
+
+        //创选项下面的条目对象
+        JMenuItem replayItem = new JMenuItem("重新登录游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登录游戏");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem = new JMenuItem("公众号");
+
+        //将每一个选项下面的条目天极爱到选项中
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        //将菜单里面的两个选项添加到菜单中
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+        //给整个界面设置菜单
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initJFram() {
         //设置界面宽高
         this.setSize(600,680);
 
@@ -23,8 +64,5 @@ public class GameJFrame extends JFrame {
 
         //设置关闭模式(参数是底层定义的常量)
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //设置界面显示
-        this.setVisible(true);
     }
 }
